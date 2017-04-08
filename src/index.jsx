582d5a6488260cf27c448df332adeb6ee1c2fe89
blog-lyn <a@s76.org>
2017-04-08 22:43:51 +0800

    changes

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import Rx from 'rxjs';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+const input = Rx.Observable.fromEvent(document.querySelector('input'), 'input');
+input.filter(event => event.target.value.length > 1)
+  .map(event => event.target.value)
+  .throttleTime(1500)
+  //.debounceTime(500)
+  //.distinctUntilChanged()
+  .subscribe(value => console.log(value));
